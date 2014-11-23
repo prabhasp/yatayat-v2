2907e54ecbd3ce4a6269eaac958d968144b204e2
prabhasp <prabhas.pokharel@gmail.com>
2014-11-23 23:30:04 +0545

    Basic node,way,relation parsing into maps

--- src/clj/yy/osm_parse.clj
+++ src/clj/yy/osm_parse.clj
@@ -7,32 +7,51 @@
   "Takes a string or filename and produces an xml-zipper out of it."
   (zip/xml-zip (xml/parse x)))
 
+(defn kv-map [zpr]
+  "Take a zipped thing derived from osm-xml, and returns k->v map."
+  (zipmap (map keyword (zx/xml-> zpr :tag (zx/attr :k)))
+          (zx/xml-> zpr :tag (zx/attr :v))))
+
 (defn z->node [n]
   "Take a node in zip form and makes it into a yy node"
-  (let [ks (map keyword (zx/xml-> n :tag (zx/attr :k)))
-        vs (zx/xml-> n :tag (zx/attr :v))]
-    (merge {:type :node}
+  (merge {:type :node}
            (:attrs (first n))
-           (zipmap ks vs))))
+           (kv-map n)))
+
+(defn z->way [nodemap w]
+  "Take a way in zip form and make it into a yy node.
+   Also needs a nodemap that maps ids to node objects."
+  (let [nodes (select-keys nodemap (zx/xml-> w :nd (zx/attr :ref)))]
+    (merge {:type :way :nodes nodes} (kv-map w))))
+
+(defn z->relation [nodemap waymap r]
+  "Take a way in zip form and make it into a yy node.
+   Also needs a nodemap,waymap that map from id to node,way."
+  (let [nodes (select-keys nodemap (zx/xml-> r :member (zx/attr :ref)))
+        ways (select-keys waymap (zx/xml-> r :member (zx/attr :ref)))]
+    (merge {:type :way :nodes nodes :ways ways} (kv-map r))))
+
+(defn- z->map [zpr typ f]
+  "Take a zipper from xml, and extracts a map for typ :node|:way|:relation,
+   and f which takes zpr and maps to the required object (eg. z->node)."
+    (into {}
+          (for [t (zx/xml-> zpr typ)]
+            (vector (zx/attr t :id) (f t)))))
+(defn z->nodemap [zpr]
+  (z->map zpr :node z->node))
+(defn z->waymap [nodemap zpr]
+  (z->map zpr :way (partial z->way nodemap)))
+(defn z->relationmap [nodemap waymap zpr]
+  (z->map zpr :relation (partial z->relation nodemap waymap)))
 
 (defn xml->yy [fname]
   "INCOMPLETE function to map from xml to a clojure-idiomatic data structure."
   (let [root (-> fname xml/parse zip/xml-zip)
-        nodes (zx/xml-> root :node)
-        nodemap (into {}
-                      (for [n nodes]
-                        (vector (zx/attr n :id) (z->node n))))
-        ways (zx/xml-> root :way)
-        w->way (fn [w] (zx/xml-> w :nd (zx/attr :ref) #(get nodemap %)))
-        waymap (into {}
-                     (for [w ways]
-                       (vector (zx/attr w :id)
-                               (merge {:type :way}
-                                      (:attrs (first w))
-                                      {:nodes (w->way w)}))))
+        nodemap (z->nodemap root)
+        waymap (z->waymap nodemap root)
         relations (zx/xml-> root :relation)]
-    (str (count nodes) " nodes, " (count ways) " ways,"
+    (str (count nodemap) " nodes, " (count waymap) " ways,"
          " and " (count relations) " relations present.")
-    {:ways ways :relations relations :nodes nodes :nodemap nodemap :waymap waymap}))
+    {:ways waymap :relations relations :nodemap nodemap}))
 
 (def test-obj (xml->yy "resources/public/data/transit.stable.xml"))
--- test/clj/yy/osm_parse_test.clj
+++ test/clj/yy/osm_parse_test.clj
@@ -2,19 +2,88 @@
   (:require [midje.sweet :refer :all]
             [yy.osm-parse :refer :all]
             [clojure.zip :as zip]
-            [clojure.xml :as xml]))
-
+            [clojure.xml :as xml]
+            [clojure.data.zip.xml :as zx]))
 
 (defn zip-str [s]
   (zip/xml-zip
    (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
-(facts "xml->node works correctly."
-       (let [nodexml "<node id=\"31150627\" lat=\"27.6984315\" lon=\"85.3485208\">
-             <tag k=\"name\" v=\"Sinamangal\"/>
-             <tag k=\"public_transport\" v=\"stop_position\"/>
-             </node>"
-             n (zip-str nodexml)]
-         (z->node n) => {:type :node
-                         :id "31150627" :lat "27.6984315" :lon "85.3485208"
-                         :name "Sinamangal" :public_transport "stop_position"}))
+(def test-xml
+  "<?xml version='1.0' encoding='UTF-8'?>
+  <osm version='0.6' generator='Overpass API'>
+  <note>The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.</note>
+  <meta osm_base='2013-07-23T23:59:02Z'/>
+  <node id='31150627' lat='27.6984315' lon='85.3485208'>
+    <tag k='name' v='Sinamangal'/>
+    <tag k='public_transport' v='stop_position'/>
+  </node>
+  <node id='1347468314' lat='27.6989046' lon='85.3478461'/>
+  <node id='1347468329' lat='27.6992615' lon='85.3474024'/>
+  <node id='2296825748' lat='27.6992366' lon='85.3473930'/>
+  <node id='1347468354' lat='27.6991855' lon='85.3474211'/>
+  <node id='1347468309' lat='27.6986671' lon='85.3482406'/>
+  <node id='2007675777' lat='27.6985687' lon='85.3483866'/>
+  <node id='31228768' lat='27.6789154' lon='85.3495276'>
+    <tag k='name' v='Koteshwor Stop'/>
+    <tag k='public_transport' v='stop_position'/>
+  </node>
+  <node id='1274184084' lat='27.7037097' lon='85.3164292'>
+    <tag k='name' v='Purano Bus Park'/>
+    <tag k='public_transport' v='stop_position'/>
+  </node>
+  <way id='120105828'>
+    <nd ref='1347468329'/>
+    <nd ref='2296825748'/>
+    <nd ref='1347468354'/>
+    <nd ref='1347468314'/>
+    <nd ref='1347468309'/>
+    <nd ref='2007675777'/>
+    <nd ref='31150627'/>
+    <tag k='highway' v='tertiary'/>
+    <tag k='name' v='Pashupati Marga'/>
+  </way>
+  <relation id='2277212'>
+    <member type='node' ref='31228768' role='terminus'/>
+    <member type='node' ref='1274184084' role='terminus'/>
+    <member type='way' ref='120105828' role=''/>
+    <tag k='ref' v='annapurna B'/>
+    <tag k='route' v='tempo'/>
+    <tag k='type' v='route'/>
+  </relation>
+  </osm>")
+
+(let [root (zip-str test-xml)
+      node1 (z->node (zx/xml1-> root :node))
+      nmap (z->nodemap root)
+      way1 (z->way nmap (zx/xml1-> root :way))
+      wmap (z->waymap nmap root)
+      rel1 (z->relation nmap wmap (zx/xml1-> root :relation))
+      rmap (z->relationmap nmap wmap root)]
+  (facts "xml->node parsing works correctly."
+         node1 => {:type :node
+                   :id "31150627" :lat "27.6984315" :lon "85.3485208"
+                   :name "Sinamangal" :public_transport "stop_position"})
+  (facts "xml->nodemap generation works correctly."
+         (sort (keys nmap)) => (sort ["31150627" "1347468314" "1347468329"
+                                      "2296825748" "1347468354" "1347468309"
+                                      "2007675777" "31228768" "1274184084"])
+         (nmap "31150627") => node1)
+  (facts "xml->way parsing works correctly."
+        (:highway way1) => "tertiary"
+        (:name way1) => "Pashupati Marga"
+        (:type way1) => :way
+        (count (:nodes way1)) => 7
+        ((:nodes way1) "31150627") => node1)
+  (facts "xml->way parsing works correctly."
+         (keys wmap) => ["120105828"]
+         (wmap "120105828") => way1)
+  (facts "xml->relation parsing works correctly."
+         (:ref rel1) => "annapurna B"
+         (:route rel1) => "tempo"
+         (:type rel1) => "route"
+         (:nodes rel1) => (select-keys nmap #{"31228768" "1274184084"})
+         (:ways rel1) => wmap)
+  (facts "xml->relationmap generation works correctly."
+         (keys rmap) => ["2277212"]
+         (rmap "2277212") => rel1))
